df2dcbd64bafc20665964817007c5b02322e2b46
Ермаков Денис <DEErmakov@lanit.ru>
2023-08-21 13:21:47 +0300

    Skeleton

---
+++ hazelcast-poc/src/test/java/poc/hazelcast/HazelcastPocApplicationTests.java
@@ -0,0 +1,12 @@
+package poc.hazelcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelcastPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
